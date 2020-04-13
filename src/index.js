1ac2c9f7962232a327b9d9f4d6a7a8b003edcc29
EINazare <emanuelioannazare@gmail.com>
2020-04-13 17:32:55 +0300

    feature: add index exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import Alert from "./Alert";
+import Alert from "./Alert.js";
+import Dropdown from "./Dropdown.js";
+import DropdownItem from "./DropdownItem.js";
+import DropdownMenu from "./DropdownMenu.js";
+import DropdownToggle from "./DropdownToggle.js";
+
+export {
+  Alert,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle
+};
