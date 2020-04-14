6ca6095d190246f0bff0a5467922c7b7282db36b
EINazare <emanuelioannazare@gmail.com>
2020-04-14 12:32:26 +0300

    chore: add button export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Alert from "./Alert.js";
+import Button from "./Alert.js";
 import Dropdown from "./Dropdown.js";
 import DropdownItem from "./DropdownItem.js";
 import DropdownMenu from "./DropdownMenu.js";
 import DropdownToggle from "./DropdownToggle.js";
 
-export { Alert, Dropdown, DropdownItem, DropdownMenu, DropdownToggle };
+export { Alert, Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle };
