8c892b0b5e5dc9b768702817a8b6e39b18641764
EINazare <emanuelioannazare@gmail.com>
2020-04-14 12:33:33 +0300

    chore: add tooltips and popover exports

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import Dropdown from "./Dropdown.js";
 import DropdownItem from "./DropdownItem.js";
 import DropdownMenu from "./DropdownMenu.js";
 import DropdownToggle from "./DropdownToggle.js";
+import Popover from "./Popover.js";
+import Tooltip from "./Tooltip.js";
 
-export { Alert, Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle };
+export {
+  Alert,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Popover,
+  Tooltip
+};
