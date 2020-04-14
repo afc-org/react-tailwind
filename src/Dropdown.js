40c8082ddd7b6f9a039f7b81eaef6017117cde34
EINazare <emanuelioannazare@gmail.com>
2020-04-14 10:44:41 +0300

    feature: add controlled option

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -2,35 +2,44 @@ import React from "react";
 import PropTypes from "prop-types";
 import { createPopper } from "@popperjs/core";
 
-const Dropdown = ({ placement, children }) => {
+const Dropdown = ({ controlled, placement, children }) => {
   const [show, setShow] = React.useState(false);
-  const toggleDropdown = () => {
-    if (!show) {
-      createPopper(togglerRef.current, menuRef.current, {
-        placement: placement
-      });
-    }
-    setShow(!show);
-  };
-  let togglerRef = React.createRef();
-  let menuRef = React.createRef();
-  let toggler = React.cloneElement(children[0], {
-    onClick: () => toggleDropdown(),
-    ref: togglerRef
-  });
-  let menu = React.cloneElement(children[1], { show: show, ref: menuRef });
-  return (
-    <>
+  if(controlled) {
+    return (
       <div className="relative">
-        {toggler}
-        {menu}
+        {children}
       </div>
-    </>
-  );
+    );
+  } else {
+    const toggleDropdown = () => {
+      if (!show) {
+        createPopper(togglerRef.current, menuRef.current, {
+          placement: placement
+        });
+      }
+      setShow(!show);
+    };
+    let togglerRef = React.createRef();
+    let menuRef = React.createRef();
+    let toggler = React.cloneElement(children[0], {
+      onClick: () => toggleDropdown(),
+      ref: togglerRef
+    });
+    let menu = React.cloneElement(children[1], { show: show, ref: menuRef });
+    return (
+      <>
+        <div className="relative">
+          {toggler}
+          {menu}
+        </div>
+      </>
+    );
+  }
 };
 
 Dropdown.defaultProps = {
-  placement: "bottom"
+  placement: "bottom",
+  controlled: false
 };
 
 Dropdown.propTypes = {
@@ -39,7 +48,15 @@ Dropdown.propTypes = {
   //    on the choosen placement, PopperJS will rendered it
   //    where it has place
   placement: PropTypes.oneOf(["top", "bottom", "left", "right"]),
-  children: PropTypes.node
+  // if you want to controll the open/close of the DropdownMenu yourself
+  controlled: PropTypes.bool,
+  // when using the uncontrolled version
+  // you need to send exactly two children
+  // - DropdownToggle
+  // and
+  // - DropdownMenu
+  // if you fail to do so, an error will be thrown
+  children: PropTypes.node.isRequired
 };
 
 export default Dropdown;
