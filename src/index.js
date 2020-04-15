388adbc809d63ed9aa31db1912ca7ace81e618b4
EINazare <emanuelioannazare@gmail.com>
2020-04-15 12:57:06 +0300

    refactor: add popover exports and correct button one

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Alert from "./Alert.js";
-import Button from "./Alert.js";
+import Button from "./Button.js";
 import Dropdown from "./Dropdown.js";
 import DropdownItem from "./DropdownItem.js";
 import DropdownMenu from "./DropdownMenu.js";
 import DropdownToggle from "./DropdownToggle.js";
 import Popover from "./Popover.js";
+import PopoverHead from "./PopoverHead.js";
+import PopoverBody from "./PopoverBody.js";
 import Tooltip from "./Tooltip.js";
 
 export {
@@ -15,5 +17,7 @@ export {
   DropdownMenu,
   DropdownToggle,
   Popover,
+  PopoverHead,
+  PopoverBody,
   Tooltip
 };
